19d1811f63a1165e2bd8c7fbbd525a10b4a84b67
Sakly Ayoub <saklyayoub@live.com>
2016-09-01 17:46:59 +0100

    renaming files

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+/* Allow Using API */
+//header("Access-Control-Allow-Origin: *");
+
+/*Global Includes*/
+include"functions.php";
+
+//localhost/api_crud/api.php?f=Authentification&p1=user&p2=user
+
+if(isset($_GET['f'])){
+	$f = $_GET['f'];
+	if(isset($_GET['p1'])){$p1 = $_GET['p1'];}
+	if(isset($_GET['p2'])){$p2 = $_GET['p2'];}
+	if(isset($_GET['p3'])){$p3 = $_GET['p3'];}
+	if(isset($_GET['p4'])){$p4 = $_GET['p4'];}
+	if(isset($_GET['p5'])){$p5 = $_GET['p5'];}
+	switch($f){
+		//Authentification
+		case "LogIn";LogIn($p1, $p2);break;
+		case "LogOut";LogOut($p1);break;
+		//Log
+		case "LogRead";LogRead($p1);break;
+		case "Help";Help();break;
+	}
+}
+?>
